4323288e70dfddd6fadb50081cf1ec67093ae733
V1adimir-Ragozin <vragozin1995@gmail.com>
2024-10-13 20:46:21 +0300

    Add files via upload

---
+++ Module16_1.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def welcome() -> str:
+    return 'Главная страница'
+
+
+@app.get('/user/admin')
+async def admin() -> str:
+    return 'Вы вошли как администратор'
+
+
+@app.get('/user/{user_id}')
+async def user(user_id: str = '123') -> str:
+    return f'Вы вошли как пользователь №{user_id}'
+
+
+@app.get('/user')
+async def user_info(username: str = 'Ilya', age: int = 25) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
