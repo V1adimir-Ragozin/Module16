48aaee295ff19635cd87764ac128537e421bdaad
V1adimir-Ragozin <vragozin1995@gmail.com>
2024-10-14 21:00:13 +0300

    Add files via upload

---
+++ Module16_3.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get('/users')
+async def get_users() -> dict:
+    return users
+
+
+@app.post('/user/{username}/{age}')
+async def add_user(
+        username: Annotated[
+            str, Path(min_length=3, max_length=15, description='Введите имя пользователя', example='UrbanUser')],
+        age: Annotated[int, Path(ge=18, le=120, description='Введите возраст', example=24)]
+) -> str:
+    user_id = str(int(max(users, key=int)) + 1)
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f'User {user_id} is registered'
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description='Введите id пользователя', example=1)],
+        username: Annotated[
+            str, Path(min_length=3, max_length=15, description='Введите имя пользователя', example='UrbanUser')],
+        age: Annotated[int, Path(ge=18, le=120, description='Введите возраст', example=22)]
+) -> str:
+    if str(user_id) in users.keys():
+        users[str(user_id)] = f'Имя: {username}, возраст: {age}'
+        return f'The user {user_id} has been updated'
+    else:
+        return f'The user ID {user_id} not found'
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description='Введите id пользователя', example=1)]) -> str:
+    try:
+        users.pop(str(user_id))
+        return f'The user {user_id} has been deleted'
+    except KeyError:
+        return f'The user ID {user_id} not found'
